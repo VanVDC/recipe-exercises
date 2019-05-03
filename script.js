e8dc83255a214520f573187c2b562697ed3fe351
VanVDC <vanbvy@gmail.com>
2019-05-02 20:29:36 -0400

    added script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+var two = 2;
+var oneFourth = "1/4"; 
+
+function marinaraSauace(cookName){
+    console.log("-hey " + cookName +"! This is my marinara sauce recipe!")
+    console.log("-Heat a medium-large saucepan over medium heat. ");
+    console.log("-Add " + oneFourth + " cup water, " + two + " cans of crushed tomates, and salt and season with black pepper to taste.");
+    console.log("-Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
+    console.log("-Stir in " + oneFourth + " cup roughly chopped fresh basil, salt and pepper as needed.");}
+
+    marinaraSauace("Van");
+
